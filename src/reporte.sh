48779e5c6d89353515315a49e2789a176adf0d75
Cesar Villafuerte <cesarcr03@gmail.com>
2016-11-05 11:59:36 -0600

    history de la clase 1

--- src/reporte.sh
+++ src/reporte.sh
@@ -1,6 +1,21 @@
+
 #!/bin/bash
 
 REPORTE=reporte-`date +%Y%m%d`.log
 
 echo "Reporte generado por $USER:" > $REPORTE
 echo " " >> $REPORTE 
+echo " " >> $REPORTE
+
+echo "##################################################" >> $REPORTE
+echo "Interfaces disponibles en el servidor" >>$REPORTE
+echo "##################################################" >> $REPORTE
+echo " " >> $REPORTE
+
+ifconfig >> $REPORTE
+
+echo "##################################################" >> $REPORTE
+echo "Interfaces disponibles del servidor" >> $REPORTE
+echo "###################################################" >>$REPORTE
+
+netstat -latup | grep LISTEN
